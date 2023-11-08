a8bc926ee8e9d16ef4abcc6096fe78ddd095aab4
q1nfo <q1nffos@gmail.com>
2023-11-09 00:33:48 +0300

    ADD route signup, login, profile

---
+++ src/core/serializers.py
@@ -0,0 +1,58 @@
+from django.contrib.auth import authenticate
+from django.contrib.auth.hashers import make_password
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+from rest_framework.exceptions import AuthenticationFailed, ValidationError
+
+from core.models import User
+
+
+class PasswordField(serializers.CharField):
+
+    def __init__(self, **kwargs):
+        kwargs['style'] = {'input_type': 'password'}
+        kwargs.setdefault('write_only', True)
+        super().__init__(**kwargs)
+        self.validators.append(validate_password)
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    password = PasswordField(required=True)
+    password_repeat = PasswordField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password', 'password_repeat']
+
+    def validate(self, attrs: dict) -> dict:
+        if attrs['password'] != attrs['password_repeat']:
+            raise ValidationError('Passwords must much')
+        return attrs
+
+    def create(self, validated_data: dict):
+        del validated_data['password_repeat']
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = PasswordField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data: dict):
+        if not (user := authenticate(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )):
+            raise AuthenticationFailed
+        return user
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'first_name', 'last_name', 'email']
